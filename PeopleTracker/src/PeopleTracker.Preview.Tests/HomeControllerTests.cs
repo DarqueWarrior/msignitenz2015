4b93c6089104281969fce28f6f20fc0886db1c33
DarqueWarrior <dbrown@microsoft.com>
2015-10-31 07:00:28 -0700

    Added xUnit

---
+++ PeopleTracker/src/PeopleTracker.Preview.Tests/HomeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace PeopleTracker.Preview.Tests
+{
+   public class HomeControllerTests
+   {
+      [Fact]
+      public void Index()
+      {
+         // Arrange
+         var target = new PeopleTracker.Preview.Controllers.HomeController();
+
+         // Act
+         var results = target.Index();
+
+         // Assert
+         Assert.NotNull(results);
+      }
+   }
+}
