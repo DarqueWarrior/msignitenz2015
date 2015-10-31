c8143262dae1477310abb7c3cfb633b9adbcb909
DarqueWarrior <dbrown@microsoft.com>
2015-10-31 10:34:26 -0700

    Added PowerShell Scripts to build and test our project.

---
+++ PeopleTracker/RunTests.ps1
@@ -0,0 +1,10 @@
+Set-ExecutionPolicy unrestricted -Scope CurrentUser -Force  
+
+# Setup the dnx and add it to your current path 
+dnvm setup 
+
+# Set runtime to use 
+dnvm use 1.0.0-beta7 
+
+# Run the test 
+dnx test 
