728de39b4ee279e4edd6a1d94d6243432e4e64b1
DarqueWarrior <dbrown@microsoft.com>
2015-12-20 20:39:03 -0800

    Changed encoding

--- PeopleTracker/src/PeopleTracker.Preview/Properties/PublishProfiles/packagesite.cmd
+++ PeopleTracker/src/PeopleTracker.Preview/Properties/PublishProfiles/packagesite.cmd
@@ -1,4 +1,4 @@
-﻿set path=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\Web Tools\External;%1\node_modules\.bin;%path%
+set path=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\Web Tools\External;%1\node_modules\.bin;%path%
 
 dnu.cmd publish "%1" --out "%2" --wwwroot-out "wwwroot"
 
